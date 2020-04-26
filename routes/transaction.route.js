cb69ecc48317d9f5eb248c3f87943a982724db3a
Glitch (hello-express) <none>
2020-04-26 10:47:31 +0000

    🍿🛩 Checkpoint ./routes/transaction.route.js:15233850/138 ./views/transactions/create.pug:15233850/396:15261052/1252

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
 });
 
 router.get("/create", (req, res) => {
-  res.render('transactions/create'),{
+  res.render('transactions/create.pug'),{
     users: db.get("users").value(),
     books: db.get("books").value(),
   }
