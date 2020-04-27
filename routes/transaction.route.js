adc3f2e0ae7db206c8793e5aa74a1a7ed9d32bc2
Glitch (hello-express) <none>
2020-04-27 06:43:38 +0000

    🚶🚤 Checkpoint ./routes/transaction.route.js:15261052/483 ./views/index.html:15261052/149 ./views/transactions/create.pug:15261052/4 ./db.json:15261052/125 ./views/transactions/index.pug:15261052/272

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -4,8 +4,15 @@ const db =require("../db.js");
 const shortid = require('shortid');
 
 router.get("/", (req, res) => {
+  var transactions = db.get('transactions').value();
+  var userId = transactions.userId;
+  var user = db.get('users').filter({ id: userId}).value();
+  var bookId = transactions.bookId;
+  var book = db.get('books').filter({ id: bookId}).value();
   res.render('transactions/index',{
-    transactions: db.get('transactions').value()    
+    transactions: transactions,
+    users: user,
+    books: book
   })
   console.log(db.get('transactions').value())
 });
@@ -23,7 +30,7 @@ router.post('/create', (req, res) => {
   req.body.id = shortid.generate();
   var userId =req.body.user;
   var bookId =req.body.book;
-  db.get('transactions').push(userId, bookId).write();
+  db.get('transactions').push(req.body).write();
   res.redirect('/transactions');
 });
 
