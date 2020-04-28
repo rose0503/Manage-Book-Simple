99ddfa5e82c4c84e2c0e2357ad32c83719451250
Glitch (hello-express) <none>
2020-04-28 10:16:51 +0000

    💺🎰 Checkpoint ./controllers/transaction.controller.js:15261052/433 ./routes/transaction.route.js:15261052/445 ./views/index.html:15261052/13

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -19,4 +19,19 @@ module.exports.create = (req, res) => {
   })
 };
 
-module.exports.complete
+module.exports.complete = (req, res) => {
+  var id = req.params.id;
+  var trans = db.get('transactions').value();
+  if(!trans.isComplete)
+    db.get('transactions').find({ id: id}).assign({ isComplete: true}).write();
+  res.redirect('/transactions');
+};
+
+
+module.exports.postCreate = (req, res) => {
+  req.body.id = shortid.generate();
+  
+  req.body.isComplete = false;
+  db.get('transactions').push(req.body).write();
+  res.redirect('/transactions');
+};
--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -8,21 +8,9 @@ router.get("/", controller.index);
 
 router.get("/create", controller.create);
 
-router.get('/:id/complete', (req, res) => {
-  var id = req.params.id;
-  var trans = db.get('transactions').value();
-  if(!trans.isComplete)
-    db.get('transactions').find({ id: id}).assign({ isComplete: true}).write();
-  res.redirect('/transactions');
-});
+router.get('/:id/complete', controller.complete);
 
-router.post('/create', (req, res) => {
-  req.body.id = shortid.generate();
-  
-  req.body.isComplete = false;
-  db.get('transactions').push(req.body).write();
-  res.redirect('/transactions');
-});
+router.post('/create', controller.postCreate);
 
 
 module.exports = router; 
