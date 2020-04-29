ec3b4cf87f6900b50740b5244d0c9c1815ca293f
Glitch (hello-express) <none>
2020-04-29 18:03:24 +0000

    🙉💏 Checkpoint ./controllers/auth.controller.js:15261052/681 ./db.json:15261052/2830

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,34 @@
+const db =require("../db.js");
+const shortid = require('shortid');
+
+
 module.exports.login =  (req, res) => {
   res.render('auth/login')
 }
 
 module.exports.postLogin = (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
   
+  var user = db.get("users").find({email: email}).value();
+  
+  if(!user){
+    res.render("auth/login",{
+      errors : [
+        "Email is not exist."
+      ],
+      values: req.body
+    })
+    return;
+  }
+  if(user.password !== password){
+    res.render("auth/login",{
+      errors: [
+        "Wrong password."
+      ],
+      values: req.body
+    })
+    return;
+  }
+  res.redirect("/")
 }
