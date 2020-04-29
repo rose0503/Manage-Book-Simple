7ab29daae8a34ae0d42ce5c713667fc8787abcfe
Glitch (hello-express) <none>
2020-04-29 20:22:06 +0000

    💐🕰 Checkpoint ./controllers/index.controller.js:15261052/191 ./views/index.pug:15261052/19 ./views/layout/common.pug:15261052/1

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -2,7 +2,8 @@ const db =require("../db.js");
 const shortid = require('shortid');
 
 module.exports.index =  (request, response) => {
+  var user = db.get("users").find({id: request.cookies.userId}).value();
   response.render("index", {
-    icon: req.cookies.userId
+    user: user
   });
 }
