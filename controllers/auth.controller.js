14dcc4c209bde1a7c54cf1ded161b7341f7c1eee
Glitch (hello-express) <none>
2020-05-01 18:53:30 +0000

    🦄🍛 Checkpoint ./controllers/auth.controller.js:15261052/5987 ./server.js:15261052/142

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,63 +1,84 @@
-//const md5 = require("md5"); 
+//const md5 = require("md5");
 const sgMail = require("@sendgrid/mail");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-const db =require("../db.js");
-const shortid = require('shortid');
+const db = require("../db.js");
+const shortid = require("shortid");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-
-module.exports.login =  (req, res) => {
-  res.render('auth/login')
-}
+module.exports.login = (req, res) => {
+  res.render("auth/login");
+};
 
 module.exports.postLogin = (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
-  
-  var user = db.get("users").find({email: email}).value();
-  
-  if(!user){
-    res.render("auth/login",{
-      errors : [
-        "Email is not exist."
-      ],
+
+  var user = db
+    .get("users")
+    .find({ email: email })
+    .value();
+
+  if (!user) {
+    res.render("auth/login", {
+      errors: ["Email is not exist."],
       values: req.body
-    })
+    });
     return;
   }
-  
+
   let countWrongPassword = user.wrongLoginCount;
   if (countWrongPassword >= 3) {
-    res.render("auth/login",{
-      errors : [
-        "Bạn đã nhập sai quá nhiều lần. Hãy quay lại sau!!."
-      ],
+    res.render("auth/login", {
+      errors: ["Bạn đã nhập sai quá nhiều lần. Hãy quay lại sau!!."],
       values: req.body
-    })
+    });
+
+    const msg = {
+      to: email,
+      from: "tqviet.0503@gmail.com",
+      subject:
+        "Enter the wrong password more than the specified number of times",
+      text:
+        "If you receive this email, because you have entered the wrong password more than 3 times. The account will be reopened in 24 hours after receiving this email. If you want to reopen your account quickly or contact us via email (lekhachai9999@gmail.com)",
+      html:
+        "If you receive this email, because you have entered the wrong password more than 3 times. The account will be reopened in 24 hours after receiving this email. If you want to reopen your account quickly or contact us via email (lekhachai9999@gmail.com)"
+    };
+    sgMail.send(msg).then(
+      () => {},
+      error => {
+        console.error(error);
+
+        if (error.response) {
+          console.error(error.response.body);
+        }
+      }
+    );
   }
-      
-  //var hashedPassword = md5(password);  
+
+  //var hashedPassword = md5(password);
   const resultCheck = bcrypt.compareSync(password, user.password);
-  
-  if(resultCheck === false){
-    db.get("users").find({email: email}).assign({ wrongLoginCount: (countWrongPassword+=1)}).write()
-    res.render("auth/login",{
-      errors: [
-        "Wrong password."
-      ],
+
+  if (resultCheck === false) {
+    db.get("users")
+      .find({ email: email })
+      .assign({ wrongLoginCount: (countWrongPassword += 1) })
+      .write();
+    res.render("auth/login", {
+      errors: ["Wrong password."],
       values: req.body
-    })
+    });
     return;
   }
-  
-  res.cookie("userId", user.id, { signed: true});
-  res.redirect("/")
-}
+
+  res.cookie("userId", user.id, { signed: true });
+  res.redirect("/");
+};
 
 module.exports.logout = (req, res) => {
-    res.clearCookie("userId", {
-      path: "/"
-    });
-    res.redirect("/auth/login");
-  }
+  res.clearCookie("userId", {
+    path: "/"
+  });
+  res.redirect("/auth/login");
+};
