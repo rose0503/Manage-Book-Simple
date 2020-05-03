c157248692f959c13023692655db28652ceacd35
Glitch (hello-express) <none>
2020-05-03 10:38:56 +0000

    🎐🌮 Checkpoint ./middlewares/cart.middleware.js:15261052/271 ./db.json:15261052/8

--- middlewares/cart.middleware.js
+++ middlewares/cart.middleware.js
@@ -1,21 +1,16 @@
 const db = require("../db")
 
 module.exports = {
-  cart:  (req, res, next) => {    
-    // var id = req.params.id;
-  var id = req.params.id;
+  cart:  (req, res, next) => {   
   var sessionId = req.signedCookies.sessionId;
   var session=  db.get('sessions').find({id: sessionId}).value()
   
-  var count = db.get('sessions').find({id: sessionId}).get('cart.' + id, 0).value();
-  console.log("count",count);
-  db.get('sessions').find({id: sessionId}).set("cart." + id, count + 1).write();
   const cartArr = session.cart;
   //console.log("cartid", session.cart)
   let result = 0;
   for(let a of Object.keys(cartArr))
     result += cartArr[a];
   res.locals.countBooks = result;
-    next();
+  next();
   }
 };
