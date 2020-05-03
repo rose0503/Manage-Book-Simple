d5352dbae99aae5ee4e188efe8ad43a5aed9bb10
Glitch (hello-express) <none>
2020-05-03 11:25:57 +0000

    🍩🏖 Checkpoint ./views/books/create.pug:15261052/149 ./validations/book.validate.js:15261052/1057

--- validations/book.validate.js
+++ validations/book.validate.js
@@ -7,14 +7,39 @@
   if(!req.body.description){
     error.push('Vui lòng nhập mô tả.');
   }
-  if(error.length){
+     
+  if (!req.file) {
+    error.push('Image is required');
+      //throw new Exception("Image is required");
+    }
+    if (!checkIsImage(req.file.mimetype)) {
+      error.push('Image is not valid');
+      //throw new Exception("Image is not valid");
+    }
+
+    const path =  cloudinary.uploader
+      .upload(req.file.path, {
+        public_id: `student/${req.file.filename}`,
+        tags: "student"
+      })
+      .then(result => result.url)
+      .catch(_ => false);
+    if (!path) throw new Exception("There was an error saving your image");
+
+    const newBook = { title, description, id: shortId(), imgUrl: path };
+    db.get("books")
+      .push(newBook)
+      .write();
+    // add dum data
+    fs.unlinkSync(req.file.path);
+   
+   if(error.length){
     res.render('books/create',{
       errors: error ,
       values: req.body
     })
     return;
   }
-   
    next();
  };
 
