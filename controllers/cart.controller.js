4b08a1a6199a93e9b0fdebeb005afdfa0400cfb2
Glitch (hello-express) <none>
2020-05-03 09:16:56 +0000

    ⛵️🐟 Checkpoint ./controllers/cart.controller.js:15261052/1357

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -4,13 +4,31 @@ module.exports.addToCart = (req, res) =>{
   var id = req.params.id;
   var sessionId = req.signedCookies.sessionId;
   
-  if(!sessionId){
-    res.redirect('/books')
-    return;
-  }
-  var count = db.get('sessions').find({id: sessionId}).get('cart.count' + id, 0).value();
-  
-  db.get('sessions').find({id: sessionId}).set("cart.count" + id, count + 1).write();
+  var session = db.get('sessions').value()
+  // if(!sessionId){
+  //   res.redirect('/books')
+  //   return;
+  // }
+  // var count = db.get('sessions').find({id: sessionId}).get('cart.boodId:' + id, 0).value();
+  // console.log("count", count);
+  // db.get('sessions').find({id: sessionId}).set("cart.bookId:" + id,"count:" +  count + 1).write();
+   if(!sessionId){
+     let newSes = new session();
+         newSes.id = sessionId;
+         newSes.cart = [{ bookId: id, count: 1 }];
+         newSes.push()
+    } else {
+      let listCart = doc.cart;
+      // found bookId
+      let index = listCart.findIndex(x => x.bookId === bookId);
+      // not found
+      if (index !== -1) {
+        listCart[index].count += 1;
+      } else {
+        listCart.push({ bookId: bookId, count: 1 });
+      }
+      doc.save();
+
   res.redirect('/books');
   
 };
