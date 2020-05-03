1f6a22404b2b5d8e58bd2167f19aab179fbdf641
Glitch (hello-express) <none>
2020-05-03 10:03:56 +0000

    🛋🌌 Checkpoint ./middlewares/cart.middleware.js:15261052/272 ./controllers/cart.controller.js:15261052/287 ./db.json:15261052/159

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -3,14 +3,19 @@ const db =require("../db.js");
 module.exports.addToCart = (req, res) =>{
   var id = req.params.id;
   var sessionId = req.signedCookies.sessionId;
-  
+  var session=  db.get('sessions').find({id: sessionId}).value()
   if(!sessionId){
     res.redirect('/books')
     return;
   }
   var count = db.get('sessions').find({id: sessionId}).get('cart.' + id, 0).value();
-  
+  console.log("count",count);
   db.get('sessions').find({id: sessionId}).set("cart." + id, count + 1).write();
+  // let result = session.reduce((acc, cur) => {
+  //       return (acc += cur[1]);
+  //     }, 0);
+  // res.locals.countBooks = result;
+  
   res.redirect('/books');
   
 };
--- middlewares/cart.middleware.js
+++ middlewares/cart.middleware.js
@@ -2,18 +2,18 @@ const db = require("../db")
 
 module.exports = {
   cart:  (req, res, next) => {    
-    const id = req.signedCookies.sessionId;
-    const session= db.get("sessions").find({id : id}).value();
-    // count book add cart
-     //sessions.find({id : id}).then(doc => {
-    
-      const cartArr = session.cart ? session.cart : [];
-      let result = cartArr.reduce((acc, cur) => {
-        return (acc += cur[1]);
-      }, 0);
-      res.locals.countBooks = result;
-    //});
-    console.log(cartArr)
+    // const id = req.signedCookies.sessionId;
+    // const session= db.get("sessions").find({id : id}).value();
+    // // count book add cart
+    //  //sessions.find({id : id}).then(doc => {
+    //   var count = session.get('cart.' + id).value();
+    //   const cartArr = session.cart ? session.cart : [];
+    //   let result = cartArr.reduce((acc, cur) => {
+    //     return (acc += cur[1]);
+    //   }, 0);
+    //   res.locals.countBooks = result;
+    // //});
+    // console.log(cartArr)
     next();
   }
 };
