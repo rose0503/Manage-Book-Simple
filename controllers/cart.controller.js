9b29d83b4cfcb0e22127152229935e2a41ea9204
Glitch (hello-express) <none>
2020-05-03 06:30:56 +0000

    🙇👄 Checkpoint ./controllers/cart.controller.js:15261052/287

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,3 +1,5 @@
+const db =require("../db.js");
+
 module.exports.addToCart = (req, res) =>{
   var id = req.params.id;
   var sessionId = req.signedCookies.sessionId;
@@ -6,4 +8,8 @@ module.exports.addToCart = (req, res) =>{
     res.redirect('/books')
     return;
   }
+  var count = db.get('sessions').find({id: sessionId}).get('cart.' + id, 0).value();
+  
+  db.get('sessions').find({id: sessionId}).set("cart." + id, count + 1).write();
+  res.redirect('/books');
 };
