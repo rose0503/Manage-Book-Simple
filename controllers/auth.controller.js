e99e74d816317b9ae95531b5be2d4e680c303139
Glitch (hello-express) <none>
2020-05-11 16:12:44 +0000

    🚣🎺 Checkpoint ./controllers/auth.controller.js:15261052/234

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,74 +5,79 @@ const saltRounds = 10;
 
 const db = require("../db.js");
 const shortid = require("shortid");
+var User = require("../model/user.model");
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports.login = (req, res) => {
   res.render("auth/login");
 };
 
-module.exports.postLogin = (req, res) => {
+module.exports.postLogin = async (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
 
-  var user = db
-    .get("users")
-    .find({ email: email })
-    .value();
+//   var user = db
+//     .get("users")
+//     .find({ email: email })
+//     .value();
 
-  if (!user) {
-    res.render("auth/login", {
-      errors: ["Email is not exist."],
-      values: req.body
-    });
-    return;
-  }
+//   if (!user) {
+//     res.render("auth/login", {
+//       errors: ["Email is not exist."],
+//       values: req.body
+//     });
+//     return;
+//   }
 
-  let countWrongPassword = user.wrongLoginCount;
-  if (countWrongPassword >= 3) {
-    res.render("auth/login", {
-      errors: ["Bạn đã nhập sai quá nhiều lần. Hãy quay lại sau!!."],
-      values: req.body
-    });
+//   let countWrongPassword = user.wrongLoginCount;
+//   if (countWrongPassword >= 3) {
+//     res.render("auth/login", {
+//       errors: ["Bạn đã nhập sai quá nhiều lần. Hãy quay lại sau!!."],
+//       values: req.body
+//     });
 
-    const msg = {
-      to: email,
-      from: "tqviet.0503@gmail.com",
-      subject:
-        "Enter the wrong password more than the specified number of times",
-      text:
-        "If you receive this email, because you have entered the wrong password more than 3 times.",
-      html:
-        "If you receive this email, because you have entered the wrong password more than 3 times."
-    };
-    sgMail.send(msg).then(
-      () => {},
-      error => {
-        console.error(error);
+//     const msg = {
+//       to: email,
+//       from: "tqviet.0503@gmail.com",
+//       subject:
+//         "Enter the wrong password more than the specified number of times",
+//       text:
+//         "If you receive this email, because you have entered the wrong password more than 3 times.",
+//       html:
+//         "If you receive this email, because you have entered the wrong password more than 3 times."
+//     };
+//     sgMail.send(msg).then(
+//       () => {},
+//       error => {
+//         console.error(error);
 
-        if (error.response) {
-          console.error(error.response.body);
-        }
-      }
-    );
-  }
+//         if (error.response) {
+//           console.error(error.response.body);
+//         }
+//       }
+//     );
+//   }
 
-  //var hashedPassword = md5(password);
-  const resultCheck = bcrypt.compareSync(password, user.password);
+//   //var hashedPassword = md5(password);
+//   const resultCheck = bcrypt.compareSync(password, user.password);
 
-  if (resultCheck === false) {
-    db.get("users")
-      .find({ email: email })
-      .assign({ wrongLoginCount: (countWrongPassword += 1) })
-      .write();
-    res.render("auth/login", {
-      errors: ["Wrong password."],
-      values: req.body
-    });
-    return;
-  }
+//   if (resultCheck === false) {
+//     db.get("users")
+//       .find({ email: email })
+//       .assign({ wrongLoginCount: (countWrongPassword += 1) })
+//       .write();
+//     res.render("auth/login", {
+//       errors: ["Wrong password."],
+//       values: req.body
+//     });
+//     return;
+//   }
 
-  res.cookie("userId", user.id, { signed: true });
+//   res.cookie("userId", user.id, { signed: true });
+
+  await 
+  
   res.redirect("/");
 };
 
