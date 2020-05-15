4dd7124343105c015f7ddab3fc80c1b30cfc2e8d
Glitch (hello-express) <none>
2020-05-15 10:53:10 +0000

    🐁🎁 Checkpoint ./middlewares/session.middleware.js:15261052/63 ./controllers/cart.controller.js:15261052/1746 ./views/cart/cart.pug:15261052/6622 ./middlewares/cart.middleware.js:15261052/2447

--- middlewares/session.middleware.js
+++ middlewares/session.middleware.js
@@ -9,15 +9,15 @@ module.exports.session = async (req, res, next) => {
   if (!req.signedCookies.sessionId) {
     res.cookie("sessionId", sessionId, { signed: true });
   }
-  if(sessions.id != sessionId){
-      var newSession = new Session({
-      id: sessionId,
-      cart: {}
-    });
-    await newSession.save();
-    //db.get("sessions").push({ id: sessionId }).write();
-    console.log("newSessionId" , newSession)  
-  }
+  // if(sessions.id != sessionId){
+  //     var newSession = new Session({
+  //     id: sessionId,
+  //     cart: {}
+  //   });
+  //   await newSession.save();
+  //   //db.get("sessions").push({ id: sessionId }).write();
+  //   console.log("newSessionId" , newSession)  
+  // }
   
   next();
 };
