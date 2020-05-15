64cd84305bd3212f892fcd8c9e520b14931ca923
Glitch (hello-express) <none>
2020-05-15 06:22:01 +0000

    🎂💽 Checkpoint ./controllers/transaction.controller.js:15261052/28654

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -42,114 +42,35 @@ function generatePagination(page, paginationSizes, numPages) {
     return pageLinks;
   };
 
-// module.exports.index = async (req, res) => {
-//   // const users = db.get("users").value();
-//   const users = await User.find({});
-//   // const books = db.get("books").value();
-//   const books = await Book .find({});
-//   // let transactions = db.get("transactions").value();
-//   const transactions = await Transaction.find({})
-//   let { page, limit } = req.query;
-//   page = +page && +page >= 0 ? +page : 0;
-//   limit = +limit && +limit >= 0 ? +limit : 4;
-//   let pagination = null;
-
-//   if (transactions.length > 0) {    
-
-//     // pagination
-//     const length = transactions.length;
-//     // num of pages
-//     const numPages = Math.ceil(length / limit);
-
-//     // size of a pagination bar: default 5
-//     const paginationSizes = numPages >= 4 ? 4 : numPages;
-//     if (page >= numPages) {
-//       page = numPages - 1;
-//     }
-//     // skip
-//     const skip = page * limit;
-//     //transactions = transactions.slice(skip, skip + limit);
-//     transactions = await Transaction.find({}, null, { limit, skip });
-//     const links = generatePagination(page, paginationSizes, numPages);
-//     pagination = {
-//       links,
-//       numPages,
-//       page,
-//       limit,
-//       start: skip
-//     };
-//   }
-//   //res.render("transaction/index", { transactions, auth: req.user, pagination });
-//   res.render('transactions/index',{
-//     transactions: transactions,
-//     users: users,
-//     books: books,
-//     pagination
-//   }) 
-//   //console.log(db.get('transactions').value())
-// };
-
-
 module.exports.index = async (req, res) => {
+  // const users = db.get("users").value();
+  const users = await User.find({});
+  // const books = db.get("books").value();
+  const books = await Book .find({});
+  // let transactions = db.get("transactions").value();
+  const transactions = await Transaction.find({})
   let { page, limit } = req.query;
-  page = +page && +page >= 0 ? Math.abs(+page) : 0;
-  limit = +limit && +limit >= 0 ? Math.abs(+limit) : 3;
+  page = +page && +page >= 0 ? +page : 0;
+  limit = +limit && +limit >= 0 ? +limit : 4;
   let pagination = null;
-  const length = await Transaction.estimatedDocumentCount();
-  let transactions = [];
-  if (length > 0) {
+
+  if (transactions.length > 0) {    
+
+    // pagination
+    const length = transactions.length;
     // num of pages
     const numPages = Math.ceil(length / limit);
 
     // size of a pagination bar: default 5
-    const paginationSizes = numPages >= 5 ? 5 : numPages;
-    if (page >= numPages && numPages > 0) {
+    const paginationSizes = numPages >= 4 ? 4 : numPages;
+    if (page >= numPages) {
       page = numPages - 1;
     }
-
     // skip
     const skip = page * limit;
-    transactions = await Transaction.find({}, null, { limit, skip });
-    let { userIds, bookIds } = transactions.reduce(
-      (acc, curr) => {
-        acc.userIds
-          .add(curr.userId);
-        curr.bookIds.forEach(bookId => {
-          acc.bookIds.add(bookId);
-        });
-        return acc;
-      },
-      { userIds: new Set(), bookIds: new Set() }
-    );
-    userIds = Array.from(userIds.values());
-    bookIds = Array.from(bookIds.values());
-    const [users, books] = await Promise.all([
-      User.find({ _id: { $in: userIds } }),
-      Book.find({ _id: { $in: bookIds } })
-    ]);
-    if (req.user.isAdmin !== false) {
-      transactions = transactions.filter(trans => {
-        const { userId, isCompleted } = trans;
-        const user = users.find(val => val._id.toString() === userId);
-        if (
-          isCompleted === true ||
-          !user ||
-          user.isLogging === false ||
-          user.role === 0
-        )
-          return false;
-        return true;
-      });
-    }
-    transactions = transactions.map(trans => {
-      const { bookIds, userId, _id, isCompleted } = trans;
-      const user = users.find(val => {
-        console.log(typeof val._id.str);
-        return val._id.toString() === userId;
-      });
-      const rentedBooks = books.filter(val => bookIds.includes(val._id));
-      return { _id, user, books: rentedBooks, isCompleted };
-    });
+    transactions = transactions.slice(skip, skip + limit);
+    console.log("result transactions", transactions)
+    //transactions = await Transaction.find({}, null, { limit, skip });
     const links = generatePagination(page, paginationSizes, numPages);
     pagination = {
       links,
@@ -159,7 +80,14 @@ module.exports.index = async (req, res) => {
       start: skip
     };
   }
-  res.render("transaction/index", { transactions, auth: req.user, pagination });
+  //res.render("transaction/index", { transactions, auth: req.user, pagination });
+  res.render('transactions/index',{
+    transactions: transactions,
+    users: users,
+    books: books,
+    pagination
+  }) 
+  //console.log(db.get('transactions').value())
 };
 
 
